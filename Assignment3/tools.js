733985dac4a31f4a2ec68902437842b5eb436bd3
pervez121 <iqbalpervez121@gmail.com>
2023-02-07 03:49:37 +0500

    shopping cart added

--- Assignment3/tools.js
+++ Assignment3/tools.js
@@ -1,26 +1,49 @@
-function calculateCelsius(event){
+function calculateCelsius(event) {
     event.preventDefault();
-    var Fahrenhiet  = document.getElementById("Fahrenhiet-input").value;
+    var Fahrenhiet = document.getElementById("Fahrenhiet-input").value;
     var FahrenhietValue = parseFloat(Fahrenhiet);
     var CelciusResultDiv = document.getElementById("celciusResult");
-    if (!isNaN(FahrenhietValue)){
-        var toCelsius = ((FahrenhietValue - 32) * 5/9 )
+    if (!isNaN(FahrenhietValue)) {
+        var toCelsius = ((FahrenhietValue - 32) * 5 / 9)
         CelciusResultDiv.innerHTML += toCelsius;
     }
-    else{
-       alert("please enter a numeric value")
-    }
-}
-function calculateFahrenheit(event){
-    event.preventDefault();
-    var celsius  = document.getElementById("celsius-input").value;
-    var celsiusValue = parseFloat(celsius);
-    var fahrenhietResultDiv = document.getElementById("fahrenheitResult");
-    if (!isNaN(celsiusValue)){
-        var toFahrenhiet = ((celsiusValue * 9/5 ) + 32);
-        fahrenhietResultDiv.innerHTML += toFahrenhiet;
-    }
-    else{
+    else {
         alert("please enter a numeric value")
     }
 }
+function calculateFahrenheit(event) {
+    event.preventDefault();
+    var celsius = document.getElementById("celsius-input").value;
+    var celsiusValue = parseFloat(celsius);
+    var fahrenhietResultDiv = document.getElementById("fahrenheitResult");
+    if (!isNaN(celsiusValue)) {
+        var toFahrenhiet = ((celsiusValue * 9 / 5) + 32);
+        fahrenhietResultDiv.innerHTML += toFahrenhiet;
+    }
+    else {
+        alert("please enter a numeric value")
+    }
+}
+
+function calculatehpAmount(event){
+    event.preventDefault();
+    var hpAmount  =  parseInt(document.getElementById("hpAmount").value);
+    var envyAmount  = parseInt( document.getElementById("envyAmount").value);
+    var toshibaAmount  = parseInt( document.getElementById("toshibaAmount").value);
+    var numOfProducts = parseInt(document.getElementById("quantity").value)
+    var amountDiv = document.getElementById("amount")
+    if (!isNaN(hpAmount) && !isNaN(numOfProducts)) {
+        totalAmount = hpAmount * numOfProducts
+        amountDiv.innerHTML += "Total Cost Of Your Order Is: " + totalAmount;    
+
+    }
+    else
+    {
+        alert("enter valid values")
+    }
+
+
+
+
+}
+
