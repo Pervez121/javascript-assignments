31349c8c624d1f7089b40530f834a1142036de90
Pervez Iqbal <iqbalpervez121@gmail.com>
2023-02-06 03:00:30 -0800

    added chapter 4 assignments

--- assignment2/app.js
+++ assignment2/app.js
@@ -47,5 +47,45 @@ alert(book);
 
 // 8. Write a script to display this in browser through JS
 
-document.write("yeah! I can write HTML content throught Javascript")
+document.write("yeah! I can write HTML content throught Javascript" +"<br>")
+
+// CHAPTER 4
+
+// 1. Declare 3 variables in one statement.
+
+var hi = 1 , hello = 2, howdy = 3;
+
+// 2. Declare 5 legal & 5 illegal variable names.
+// var my veriable = 10;
+// var my%veriable = 10;
+// var alert = 10;
+// var 123 = "my name";
+// var @name = "name";
+
+// 3. Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// For example $my_1stVariable
+
+var rule1 = ("letters");
+var rule2 = ("numbers");
+var rule3 = ("dollar signs");
+var rule4 = ("underscores.");
+
+var name1 = " - ", name2 = " $ ", name3=" numbers ";
+var camelCase = "Camel Case";
+var keywords = "KeyWords"
+
+
+document.write(rule1 +"<br>"+ rule2 +"<br>"+ rule3 +"<br>"+ rule4)
+document.write("<h1>Rules for naming JS variables</h1>"+"<br>"+" <li> A: Variable names can only contain "+rule1+" "+rule2+" "+rule3+" and "+rule4+"</li><br>")
+document.write("<li>B: Variables must begin with "+name1+", "+name2+" and "+name3+", For example $name, _name or name"+"</li><br>")
+document.write("<li>C: Variables names are "+camelCase+"</li><br>")
+document.write("<li>C: Variables names Should not be JS "+keywords+"</li><br>")
+
+
+
+
+
 
