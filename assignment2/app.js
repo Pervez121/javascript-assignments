0083c450957615fc32b16e28cddf5d6786e0a62a
Pervez Iqbal <iqbalpervez121@gmail.com>
2023-02-06 00:38:54 -0800

    added chapter 2

---
+++ assignment2/app.js
@@ -0,0 +1,51 @@
+// 1. Declare a variable called username.
+var  username;
+
+// 2. Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+
+var myName = "Pervez Iqbal";
+document.write(myName);
+
+// Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+var message;
+message = "hello world";
+alert (message);
+
+// 4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+
+
+var sName = "pervez Iqbal";
+var sAge = 27;
+var sQulaification = "BS";
+alert("name of student is "+sName+" he is "+sAge+" years old "+" and his Qualification is "+sQulaification);
+
+// 5. Write a script to display the following alert using one JS variable
+
+var pizza = "pizza\npizz\npiz\npi\np"
+alert(pizza);
+
+// 6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+
+var email = "frontend921@gmail.com";
+alert("my email address is "+ email)
+
+// 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+
+var book = "A smarter way to learn JavaScript";
+alert(book);
+
+// 8. Write a script to display this in browser through JS
+
+document.write("yeah! I can write HTML content throught Javascript")
+
