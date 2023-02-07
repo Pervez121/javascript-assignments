6eebe4c51ebf4c08c23b6833fca981dd314a4a2c
pervez121 <iqbalpervez121@gmail.com>
2023-02-07 21:05:10 +0500

    assignment3 complete 6

--- Assignment3/tools.js
+++ Assignment3/tools.js
@@ -33,7 +33,8 @@ function calculatehpAmount(event){
     var numOfProducts = parseInt(document.getElementById("quantity").value)
     var amountDiv = document.getElementById("amount")
     if (!isNaN(hpAmount) && !isNaN(numOfProducts)) {
-        totalAmount = hpAmount * numOfProducts
+        console.log(hpAmount , numOfProducts)
+      var  totalAmount = hpAmount * numOfProducts
         amountDiv.innerHTML += "Total Cost Of Your Order Is: " + totalAmount;    
 
     }
@@ -47,3 +48,51 @@ function calculatehpAmount(event){
 
 }
 
+
+
+let totalMarks = 980;
+let obtainedMarks = 804;
+let percentage = (obtainedMarks/totalMarks)*100;
+
+let marksBox = document.getElementById("marks");
+
+marksBox.innerHTML += "Total Marks: "+totalMarks+"<br>"+"Obtained Marks: "+obtainedMarks+"<br>"+"Percantage: "+percentage+"%"
+
+
+
+
+let usDollers = 10;
+let saudiRiyal = 25;
+
+let pkr = (usDollers * 275) + (saudiRiyal * 73.34)
+
+let currency = document.getElementById("currency");
+currency.innerHTML += "Total Currency in PKR: "+pkr;
+
+
+
+let currentYear = new Date().getFullYear();
+let dob = 1996;
+
+const age = currentYear - dob;
+let ageBox =  document.getElementById("ageBox");
+ageBox.innerHTML += "Current Year: "+currentYear+"<br>"+"Birth Year: "+dob+"<br>"+"You Age is: "+age;
+
+
+
+
+let radius = 20;
+let circumferance = 2*3.14*radius;
+let areaOfCircle = 3.14*(radius*radius);
+
+let geomatizerBox =  document.getElementById("geomatizerBox");
+geomatizerBox.innerHTML += "Radius Of Circle: "+radius+"<br>"+"The Circumferance of Circle: "+circumferance+"<br>"+"The Area Of Circle is : "+areaOfCircle;
+
+
+let favoriteSnack = "Pizza";
+let currentAge = 26;
+let maximuxAge = 90;
+let pizzaPerMonth = 2;
+let lifeTimePizza = ((maximuxAge - currentAge)*12 ) * 2;
+let supplyCalculator = document.getElementById("supplyCalculator")
+supplyCalculator.innerHTML +="Favorite Fast Food: "+favoriteSnack+"<br>"+"Current Age: "+currentAge+"<br>"+"Estimated Maximum Age: "+maximuxAge+"<br>"+"Amount Of Snacks Per Month: "+pizzaPerMonth+"<br>"+ "“You will need "+lifeTimePizza+ " to last you until the ripe old age of "+maximuxAge;
