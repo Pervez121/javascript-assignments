bac20cb7f1338abbbb426c0d5277fc3b16da87b7
pervez121 <iqbalpervez121@gmail.com>
2023-02-10 23:19:17 +0500

    assignment 5 started

---
+++ Assignment5/app.js
@@ -0,0 +1,26 @@
+// 1. Declare an empty array using JS literal notation to store
+// student names in future.
+
+const students = [];
+console.log(students);
+
+// 2. Declare an empty array using JS object notation to store
+// student names in future.
+
+const OBstudents = new Array();
+
+// OBstudents[0] = "pervez";
+// OBstudents[1] = "Iqbal";
+// OBstudents[2] = "Shah";
+// OBstudents[3] = "Hunzai";
+
+// 3. Declare and initialize a strings array.
+var strings = ["orange", "Banna", "grapes"]
+
+// 4. Declare and initialize a numbers array.
+var booleanArray  =[ true , false , false , true]
+
+// 6. Declare and initialize a mixed array.
+
+var mixedArray = [true , 1 , "hello"]
+ 
