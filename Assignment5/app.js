6be153db226a59f0f032b46186508a644eab2b06
Pervez Iqbal <iqbalpervez121@gmail.com>
2023-02-12 10:50:41 -0800

    swapping array

--- Assignment5/app.js
+++ Assignment5/app.js
@@ -1,26 +1,69 @@
-// 1. Declare an empty array using JS literal notation to store
-// student names in future.
+// // 1. Declare an empty array using JS literal notation to store
+// // student names in future.
 
-const students = [];
-console.log(students);
+// var students = [];
+// console.log(students);
 
-// 2. Declare an empty array using JS object notation to store
-// student names in future.
+// // 2. Declare an empty array using JS object notation to store
+// // student names in future.
 
-const OBstudents = new Array();
+// const OBstudents = new Array();
 
-// OBstudents[0] = "pervez";
-// OBstudents[1] = "Iqbal";
-// OBstudents[2] = "Shah";
-// OBstudents[3] = "Hunzai";
+// // OBstudents[0] = "pervez";
+// // OBstudents[1] = "Iqbal";
+// // OBstudents[2] = "Shah";
+// // OBstudents[3] = "Hunzai";
 
-// 3. Declare and initialize a strings array.
-var strings = ["orange", "Banna", "grapes"]
+// // 3. Declare and initialize a strings array.
+// var strings = ["orange", "Banna", "grapes"]
 
-// 4. Declare and initialize a numbers array.
-var booleanArray  =[ true , false , false , true]
+// // 4. Declare and initialize a numbers array.
+// var booleanArray  =[ true , false , false , true]
 
-// 6. Declare and initialize a mixed array.
+// // 6. Declare and initialize a mixed array.
 
-var mixedArray = [true , 1 , "hello"]
+// var mixedArray = [true , 1 , "hello"]
  
+// // Declare and Initialize an array and store available
+// // education qualifications in Pakistan (e.g. SSC, HSC, BCS,
+// // BS, BCOM, MS, M. Phil., PhD). Show the listed
+// // qualifications in your browser 
+
+// var education = ["SSC", "HSC", "BCS","BS", "BCOM", "MS", "M. Phil", "PhD"]
+// console.log(education)
+// var answerSeven = document.getElementById("answerSeven");
+// answerSeven.innerHTML += "<h2> Qualifications</h3>" +"<br>"
+// for(x=0; x<education.length; x++  ){
+//     answerSeven.innerHTML += x + ") " + education[x] +  "<br>"
+// }
+
+
+// // 8. Write a program to store 3 student names in an array.Take
+// // another array to store score of these three students.
+// // Assume that total marks are 500 for each student, display
+// // the scores & percentages of students like:
+
+// var answerEight = document.getElementById("answerEight");
+// students = ["pervez" , "Iqbal" , "Shah"];
+// var score = ["320" , "230" , "480"]
+
+// answerEight.innerHTML+= "Score of "+students[0]+ " is " + score[0]+"<br>"+
+//                         "Score of "+students[1]+ " is " + score[1]+"<br>"+
+//                         "Score of "+students[2]+ " is " + score[2]+"<br>"
+
+//                         console.log(students , score)
+
+
+
+
+// swapping array
+
+        var pets = ["mouse" , "cat" , "dog"]
+
+            pets[0] = [pets[2] , pets[2] = pets[0]] [0]
+            
+
+        console.log(pets)
+
+
+
