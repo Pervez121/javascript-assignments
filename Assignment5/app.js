21d6f8d1d1a23417c3dc18fda3c75456b91123bd
Pervez Iqbal <iqbalpervez121@gmail.com>
2023-02-14 00:32:19 -0800

    working on assignment

--- Assignment5/app.js
+++ Assignment5/app.js
@@ -1,69 +1,112 @@
-// // 1. Declare an empty array using JS literal notation to store
-// // student names in future.
+// 1. Declare an empty array using JS literal notation to store
+// student names in future.
 
-// var students = [];
-// console.log(students);
+var students = [];
+console.log(students);
 
-// // 2. Declare an empty array using JS object notation to store
-// // student names in future.
+// 2. Declare an empty array using JS object notation to store
+// student names in future.
 
-// const OBstudents = new Array();
+const OBstudents = new Array();
 
-// // OBstudents[0] = "pervez";
-// // OBstudents[1] = "Iqbal";
-// // OBstudents[2] = "Shah";
-// // OBstudents[3] = "Hunzai";
+// OBstudents[0] = "pervez";
+// OBstudents[1] = "Iqbal";
+// OBstudents[2] = "Shah";
+// OBstudents[3] = "Hunzai";
 
-// // 3. Declare and initialize a strings array.
-// var strings = ["orange", "Banna", "grapes"]
+// 3. Declare and initialize a strings array.
+var strings = ["orange", "Banna", "grapes"]
 
-// // 4. Declare and initialize a numbers array.
-// var booleanArray  =[ true , false , false , true]
+// 4. Declare and initialize a numbers array.
+var booleanArray  =[ true , false , false , true]
 
-// // 6. Declare and initialize a mixed array.
+// 6. Declare and initialize a mixed array.
 
-// var mixedArray = [true , 1 , "hello"]
+var mixedArray = [true , 1 , "hello"]
  
-// // Declare and Initialize an array and store available
-// // education qualifications in Pakistan (e.g. SSC, HSC, BCS,
-// // BS, BCOM, MS, M. Phil., PhD). Show the listed
-// // qualifications in your browser 
+// Declare and Initialize an array and store available
+// education qualifications in Pakistan (e.g. SSC, HSC, BCS,
+// BS, BCOM, MS, M. Phil., PhD). Show the listed
+// qualifications in your browser 
 
-// var education = ["SSC", "HSC", "BCS","BS", "BCOM", "MS", "M. Phil", "PhD"]
-// console.log(education)
-// var answerSeven = document.getElementById("answerSeven");
-// answerSeven.innerHTML += "<h2> Qualifications</h3>" +"<br>"
-// for(x=0; x<education.length; x++  ){
-//     answerSeven.innerHTML += x + ") " + education[x] +  "<br>"
-// }
-
-
-// // 8. Write a program to store 3 student names in an array.Take
-// // another array to store score of these three students.
-// // Assume that total marks are 500 for each student, display
-// // the scores & percentages of students like:
-
-// var answerEight = document.getElementById("answerEight");
-// students = ["pervez" , "Iqbal" , "Shah"];
-// var score = ["320" , "230" , "480"]
-
-// answerEight.innerHTML+= "Score of "+students[0]+ " is " + score[0]+"<br>"+
-//                         "Score of "+students[1]+ " is " + score[1]+"<br>"+
-//                         "Score of "+students[2]+ " is " + score[2]+"<br>"
-
-//                         console.log(students , score)
+var education = ["SSC", "HSC", "BCS","BS", "BCOM", "MS", "M. Phil", "PhD"]
+console.log(education)
+var answerSeven = document.getElementById("answerSeven");
+answerSeven.innerHTML += "<h2> Qualifications</h3>" +"<br>"
+for(x=0; x<education.length; x++  ){
+    answerSeven.innerHTML += x + ") " + education[x] +  "<br>"
+}
 
 
+// 8. Write a program to store 3 student names in an array.Take
+// another array to store score of these three students.
+// Assume that total marks are 500 for each student, display
+// the scores & percentages of students like:
+
+var answerEight = document.getElementById("answerEight");
+students = ["pervez" , "Iqbal" , "Shah"];
+var score = ["320" , "230" , "480"]
+
+answerEight.innerHTML+= "Score of "+students[0]+ " is " + score[0]+ "percentage: " + ((score[0]/500)*100)+"%"+ "<br>"+
+                        "Score of "+students[1]+ " is " + score[1]+ "percentage: " + ((score[1]/500)*100)+"%"+ "<br>"+
+                        "Score of "+students[2]+ " is " + score[2]+ "percentage: " + ((score[2]/500)*100)+"%"+ "<br>"
+
+                        console.log(students , score)
 
 
-// swapping array
+// 9. Initialize an array with color names. Display the array elements in your browser.
+// a. Ask the user what color he/she wants to add to the beginning & add that color to the 
+//    beginning of the array. Display the updated array in your browser.
+// b. Ask the user what color he/she wants to add to the end & add that color to the end of
+//    the array. Display the updated array in your browser.
+// c. Add two more color to the beginning of the array. Display the updated array in your browser.
+// d. Delete the first color in the array. Display the updated array in your browser.
+// e. Delete the last color in the array. Display the updated array in your browser.
+// f. Ask the user at which index he/she wants to add a color  & color name. Then add the
+//    color to desired position/index. . Display the updated array in your browser.
+// g. Ask the user at which index he/she wants to delete color(s) & how many colors he/she
+//    wants to delete. Then remove the same number of color(s) from user-defined
+//    position/index. . Display the updated array in your browser.
 
-        var pets = ["mouse" , "cat" , "dog"]
+var colors = ["yellow" , "red" , "green" , "blue" , "brown" , "gray"]
 
-            pets[0] = [pets[2] , pets[2] = pets[0]] [0]
-            
+var answerNine = document.getElementById("answerNine");
+answerNine.innerHTML += colors + "<br>"
 
-        console.log(pets)
+var addColor = prompt("enter the name of color that you want to enter");
+
+colors.push(addColor);
+answerNine.innerHTML += colors + "<br>"
+
+addColor = prompt("add colors at the begning of array")
+colors.unshift(addColor)
+answerNine.innerHTML += colors + "<br>"
+
+
+
+delColor = prompt("do you want to delete the color from the start");
+delColor = "yes"?  colors.shift(addColor) : console.log("no color deleted")
+answerNine.innerHTML += colors + "<br>"
+
+alert("now deleteing last item");
+
+colors.pop();
+answerNine.innerHTML += colors + "<br>"
+
+delColor = prompt("at which would you like to at color from 0 to " + colors.length-1)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ swapping.js
@@ -0,0 +1,11 @@
+// swapping array
+
+var pets = ["mouse" , "cat" , "dog"]
+
+pets[0] = [pets[2] , pets[2] = pets[0]] [0]
+
+
+console.log(pets)
+
+
+
